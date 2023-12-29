d88490b0fca67b97efad8de258130398358dba9d
harshak02 <sreeharshak002@gmail.com>
2023-12-29 10:50:28 +0530

    first commit

---
+++ server/verifyToken.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import { createError } from "./error.js";
+
+export const verifyToken = async (req,res,next) => {
+    const token = req.cookies.access_token || req.body.token;
+    if(!token){
+        return next(createError(401,"Unauthenticated User!"));
+    }
+    jwt.verify(token,process.env.JWT,(err,user)=>{
+        if(err){
+            return next(createError(403,"Token is not valid!"));
+        }
+        req.user = user; //assigning user id for further applications req.user will be equal to what we set that in cookie in auth.js
+        next(); //continues from where we left 
+    })
+}
